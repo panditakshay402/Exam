2ad31e2dde50c4fd3180a5706536780a8f80d7fe
Akshay pandit <51216244+panditakshay402@users.noreply.github.com>
2023-05-07 23:43:12 +0530

    second

--- index.php
+++ index.php
@@ -1,10 +1,28 @@
 <?
 $conn=mysqli_connect("localhost","root","","student");
 
-if(!conn)
+if(!$conn)
 {
-    die("connection failed").mysqli_connect_error
+    die("connection failed".mysqli_connect_error());
 }
 
+
+if(isset($_POST['insert']))
+{
+    $id=$_POST['id'];
+    $name=$_POST['name'];
+    $age=$_POST['age'];
+
+    $sql="INSERT INTO student(id,name,age) VALUE ('$id','$name','$age')";
+
+    if(mysqli_query($conn,$sql))
+    {
+        echo("success");
+    }
+    else
+    {
+        echo("failed".mysqli_error($conn));
+    }
+}
 
 ?>
